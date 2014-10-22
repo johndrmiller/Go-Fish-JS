a64614149e2ca65d7fed9cb0071b214dc8eeee5c
johndrmiller <johndrmiller@gmail.com>
2014-10-22 19:52:30 -0400

    player update

--- js/Player.js
+++ js/Player.js
@@ -1,8 +1,10 @@
 function Player(kind){
     var kind,//String: "player" or "computer"
-        hand = [],//Array: the player's hand of cards
-        matches = [];//Array: the player's match pile
-
+        _hand = [],//Array: the player's hand of cards
+        _matches = [];//Array: the player's match pile
+        var addToHand = function (newCard) {
+							
+        }
         //player needs to be able to draw cards from pile, pick a card to ask for, reply to asked for card, check matches pile, rearrange hand, accept card from deal into hand, 
 
 }
